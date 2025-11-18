39dce3b1f251420af6eaec17925949f4d6f65162
Léo Charton <l.charton2@laposte.net>
2025-11-18 11:59:37 +0100

    game loop done

--- tetris/src/main/java/com/example/GamePanel.java
+++ tetris/src/main/java/com/example/GamePanel.java
@@ -2,16 +2,64 @@ package com.example;
 
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Graphics;
 
 import javax.swing.JPanel;
 
-public class GamePanel extends JPanel {
+public class GamePanel extends JPanel implements Runnable {
     public static final int WIDTH = 1280;
     public static final int HEIGHT = 720;
+    public static final int FPS = 60;
+    Thread gameThread;
 
     public GamePanel() {
         this.setPreferredSize(new Dimension(WIDTH, HEIGHT));
         this.setBackground(Color.black);
         this.setLayout(null);
+    }
+
+    public void launchGame() {
+        gameThread = new Thread(this);
+        gameThread.start();
+    }
+
+    @Override
+    public void run() {
+
+        // Game Loop
+        // every game loop, we update and draw
+        // update means updating position of objects, updating the score, number
+        // information
+        // drawing means drawing text or objects
+
+        double drawInterval = 1000000000 / FPS;
+        double delta = 0;
+        long lastTime = System.nanoTime();
+        long currentTime;
+
+        while (gameThread != null) {
+
+            currentTime = System.nanoTime();
+
+            delta += (currentTime - lastTime) / drawInterval;
+            lastTime = currentTime;
+
+            if (delta >= 1) {
+                update();
+                repaint();
+                delta--;
+            }
+        }
+
+    }
+
+    private void update() {
+
+    }
+
+    public void paintComponent(Graphics g) {
+
+        super.paintComponent(g);
+
     }
 }
