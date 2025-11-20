83beee9fab964af3852f68b9936cf8a4dd2d35e2
Léo Charton <l.charton2@laposte.net>
2025-11-20 12:40:11 +0100

    Mino_Z2 finished and working

--- tetris/src/main/java/com/example/mino/Mino_Z2.java
+++ tetris/src/main/java/com/example/mino/Mino_Z2.java
@@ -1,5 +1,80 @@
 package com.example.mino;
 
-public class Mino_Z2 {
+import java.awt.Color;
+
+public class Mino_Z2 extends Mino {
+
+    public Mino_Z2() {
+        create(Color.green);
+    }
+
+    public void setXY(int x, int y) {
+        //
+        // x o o
+        // o o
+
+        //
+        // xx b0 b1
+        // b3 b2
+
+        b[0].x = x;
+        b[0].y = y;
+        b[1].x = b[0].x + Block.SIZE;
+        b[1].y = b[0].y;
+        b[2].x = b[0].x;
+        b[2].y = b[0].y + Block.SIZE;
+        b[3].x = b[0].x - Block.SIZE;
+        b[3].y = b[0].y + Block.SIZE;
+    }
+
+    public void getDirection1() {
+        //
+        // x o o
+        // o o
+
+        //
+        // xx b0 b1
+        // b3 b2
+
+        temp[0].x = b[0].x;
+        temp[0].y = b[0].y;
+        temp[1].x = b[0].x + Block.SIZE;
+        temp[1].y = b[0].y;
+        temp[2].x = b[0].x;
+        temp[2].y = b[0].y + Block.SIZE;
+        temp[3].x = b[0].x - Block.SIZE;
+        temp[3].y = b[0].y + Block.SIZE;
+
+        updateXY(1);
+    }
+
+    public void getDirection2() {
+        // o
+        // o o
+        // x o
+
+        // b3
+        // b2 b0
+        // xx b1
+
+        temp[0].x = b[0].x;
+        temp[0].y = b[0].y;
+        temp[1].x = b[0].x;
+        temp[1].y = b[0].y + Block.SIZE;
+        temp[2].x = b[0].x - Block.SIZE;
+        temp[2].y = b[0].y;
+        temp[3].x = b[0].x - Block.SIZE;
+        temp[3].y = b[0].y - Block.SIZE;
+
+        updateXY(2);
+    }
+
+    public void getDirection3() {
+        getDirection1();
+    }
+
+    public void getDirection4() {
+        getDirection2();
+    }
 
 }
