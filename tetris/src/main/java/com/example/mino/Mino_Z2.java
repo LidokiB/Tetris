e6e4567843db6ff5b82352b62a64bbdb8091f0f0
Léo Charton <l.charton2@laposte.net>
2025-11-20 13:08:05 +0100

    correction on Mino_Z2, now it starts at the top of the screen

--- tetris/src/main/java/com/example/mino/Mino_Z2.java
+++ tetris/src/main/java/com/example/mino/Mino_Z2.java
@@ -14,17 +14,17 @@ public class Mino_Z2 extends Mino {
         // o o
 
         //
-        // xx b0 b1
-        // b3 b2
+        // xx b2 b3
+        // b1 b0
 
         b[0].x = x;
         b[0].y = y;
-        b[1].x = b[0].x + Block.SIZE;
+        b[1].x = b[0].x - Block.SIZE;
         b[1].y = b[0].y;
         b[2].x = b[0].x;
-        b[2].y = b[0].y + Block.SIZE;
-        b[3].x = b[0].x - Block.SIZE;
-        b[3].y = b[0].y + Block.SIZE;
+        b[2].y = b[0].y - Block.SIZE;
+        b[3].x = b[0].x + Block.SIZE;
+        b[3].y = b[0].y - Block.SIZE;
     }
 
     public void getDirection1() {
@@ -33,17 +33,17 @@ public class Mino_Z2 extends Mino {
         // o o
 
         //
-        // xx b0 b1
-        // b3 b2
+        // xx b2 b3
+        // b1 b0
 
         temp[0].x = b[0].x;
         temp[0].y = b[0].y;
-        temp[1].x = b[0].x + Block.SIZE;
+        temp[1].x = b[0].x - Block.SIZE;
         temp[1].y = b[0].y;
         temp[2].x = b[0].x;
-        temp[2].y = b[0].y + Block.SIZE;
-        temp[3].x = b[0].x - Block.SIZE;
-        temp[3].y = b[0].y + Block.SIZE;
+        temp[2].y = b[0].y - Block.SIZE;
+        temp[3].x = b[0].x + Block.SIZE;
+        temp[3].y = b[0].y - Block.SIZE;
 
         updateXY(1);
     }
@@ -53,18 +53,18 @@ public class Mino_Z2 extends Mino {
         // o o
         // x o
 
-        // b3
-        // b2 b0
-        // xx b1
+        // b1
+        // b0 b2
+        // xx b3
 
         temp[0].x = b[0].x;
         temp[0].y = b[0].y;
         temp[1].x = b[0].x;
-        temp[1].y = b[0].y + Block.SIZE;
-        temp[2].x = b[0].x - Block.SIZE;
+        temp[1].y = b[0].y - Block.SIZE;
+        temp[2].x = b[0].x + Block.SIZE;
         temp[2].y = b[0].y;
-        temp[3].x = b[0].x - Block.SIZE;
-        temp[3].y = b[0].y - Block.SIZE;
+        temp[3].x = b[0].x + Block.SIZE;
+        temp[3].y = b[0].y + Block.SIZE;
 
         updateXY(2);
     }
